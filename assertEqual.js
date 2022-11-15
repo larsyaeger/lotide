29f7e9095dc239ec63be0ebcd3fc1ab4dd5733e9
larsyaeger <larsyaeger@gmail.com>
2022-11-14 17:30:55 -0700

    Made an assertEqual function

---
+++ assertEqual.js
@@ -0,0 +1,17 @@
+function assertEqual(actual, expected) {
+  if (actual === expected) {
+    console.log('✅✅✅ Assertion Passed: ' + actual + ' === ' + expected);
+  } else {
+    console.log('🛑🛑🛑 Assertion Failed: ' + actual + ' !== ' + expected);
+  }
+}
+
+//test code
+assertEqual('Lighthouse Labs', 'Bootcamp');
+assertEqual(1, 1);
+assertEqual(-5, -5);
+assertEqual(-5, -6);
+assertEqual(-10, 0);
+assertEqual('bob', 'Bob');
+assertEqual('Bill', 'John');
+assertEqual(0, 0);
