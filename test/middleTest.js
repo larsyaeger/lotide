e3b872a46bd94df8d472009a26645a00dcd674ae
larsyaeger <larsyaeger@gmail.com>
2022-11-21 15:07:09 -0700

    small fix for middleTest.js to use deepEqual instead of strictEqual

--- test/middleTest.js
+++ test/middleTest.js
@@ -1,10 +1,10 @@
 const middle = require('../middle');
 const assertArraysEqual = require('../assertArraysEqual');
-//const assert = require('chai').assert;
+const assert = require('chai').assert;
 
 describe('#middle', () => {
   it ("arrays [1,2,3,4] and [2,2,3,6] should have the same middle values", () => {
-    assertArraysEqual(middle([1, 2, 3, 4]), middle([2, 2, 3, 6]));
+    assert.deepEqual(middle([1, 2, 3, 4]), middle([2, 2, 3, 6]));
   })
 })
 //assertArraysEqual(middle([1, 2, 3, 4]), middle([2, 2, 3, 6]));
